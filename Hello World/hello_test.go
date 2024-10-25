992d6ec0bd8f541cbe54e31baac1ff30c0914aea
Juan Camilo Herrera <juan8132juan@gmail.com>
2024-10-25 15:01:47 -0500

    Primer capitulo

--- Hello World/hello_test.go
+++ Hello World/hello_test.go
@@ -4,13 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' by default", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("en español viva goku", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say Hello in frnech", func(t *testing.T) {
+		got := Hello("Joan", "French")
+		want := "Bonjour, Joan"
 		assertCorrectMessage(t, got, want)
 	})
 }
